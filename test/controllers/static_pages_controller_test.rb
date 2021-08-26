71386e93859efa7d48eae08e1ce389a4c5849a8c
SaniaGos <saniagoss@gmail.com>
2021-08-26 15:53:04 +0300

    new_lessons

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,26 +8,32 @@ class StaticPagesControllerTest < ActionController::TestCase
 
   test "should get home" do
     get :home
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_response(:success)
+    assert_select("title", "#{@base_title}")
   end
 
   test "should get help" do
     get :help
-    assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_response(:success)
+    assert_select("title", "Help | #{@base_title}")
   end
 
   test "should get about" do
     get :about
-    assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_response(:success)
+    assert_select("title", "About | #{@base_title}")
   end
 
   test "should get contact" do
-    get :about
-    assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    get :contact
+    assert_response(:success)
+    assert_select("title", "Contact | #{@base_title}")
   end
+
+  # test "should get info" do
+  #   get(:info)
+  #   assert_response(:success)
+  #   assert_select("title", "About | #{@base_title}")
+  # end
 
 end
