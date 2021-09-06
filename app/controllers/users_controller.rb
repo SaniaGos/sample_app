3c6a1d580e9e34c7d65acf362859910a75fd9d34
SaniaGos <saniagoss@gmail.com>
2021-09-06 18:21:27 +0300

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    unless @user = User.find_by(id: params[:id])
+      render(text: "<h1>User not found</h1>", status: 404)
+    end
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save # Обработать успешное сохранение.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to(@user)  # те саме redirect_touser_url(@user)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
