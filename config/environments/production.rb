a03acdc39c516ee33ff4f883fb184e6a514744a1
SaniaGos <saniagoss@gmail.com>
2021-09-19 20:18:35 +0300

    Add password resets & email configuration1.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = "blooming-meadow-01338.herokuapp.com"
+  host = "https://blooming-meadow-01338.herokuapp.com/users"
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => "smtp.sendgrid.net",
